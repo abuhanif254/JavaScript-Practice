69cf1dbda060cdcc84e932894deedf4e6457f53f
md abu hanif mia <mohammadbitullah@gmail.com>
2026-03-14 02:44:47 +0600

    annonymouse function

--- arrow_function.js
+++ arrow_function.js
@@ -15,3 +15,18 @@ console.log(multiply(10,10));
 const divide = (a, b) => a / b;
 
 console.log(divide(100,10));
+
+// function expression and anonymous function
+
+const difference = (a,b) => a-b;
+
+const result = difference(100,50);
+
+console.log(result);
+
+
+// annonymous function in a callback
+
+const friends = ['Alice', 'Bob', 'Charlie'];
+
+friends.forEach((friend) => console.log(`hello ${friend}`));
