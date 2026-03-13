88ed8e4fac7d90577941ed8550a4c3459a291107
md abu hanif mia <mohammadbitullah@gmail.com>
2026-03-14 00:30:33 +0600

    example logical operators

---
+++ example_logical_operators..js
@@ -0,0 +1,11 @@
+const isProMembership = true;
+const isLoggedIn = true;
+
+const hasAccessCode = false;
+
+
+if((isProMembership && isLoggedIn) || hasAccessCode){
+    console.log('you can enjoi the web development course')
+}else{
+    console.log('access denied! please buy membership or course')
+};
