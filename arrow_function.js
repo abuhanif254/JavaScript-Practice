360647603483b2381ed35f041ae4ca06879bd72b
md abu hanif mia <mohammadbitullah@gmail.com>
2026-03-14 02:31:57 +0600

    use arrow function

--- arrow_function.js
+++ arrow_function.js
@@ -3,3 +3,15 @@
 const add = (a, b) => a + b;
 
 console.log(add(10,20));
+
+const subtract = (a, b) => a - b;
+
+console.log(subtract(100,50));
+
+const multiply = (a, b) => a * b;
+
+console.log(multiply(10,10));
+
+const divide = (a, b) => a / b;
+
+console.log(divide(100,10));
