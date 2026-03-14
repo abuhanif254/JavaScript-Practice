e7fe6bf1c826f7ea31a8d8a7987f4b23cb5787ba
md abu hanif mia <mohammadbitullah@gmail.com>
2026-03-15 02:22:03 +0600

    some advance javascript

---
+++ advance_javascript.js
@@ -0,0 +1,12 @@
+// the current date and time
+
+const now = new Date();
+
+console.log(now);
+
+// specific date and time
+
+const specificDate = new Date('2026-01-01')
+
+console.log(specificDate);
+
