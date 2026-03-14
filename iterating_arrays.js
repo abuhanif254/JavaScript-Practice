45b2fea0401c5b71aa30ee7a1304e0a359446595
md abu hanif mia <mohammadbitullah@gmail.com>
2026-03-14 13:58:29 +0600

    use map, foreach, lop in array

---
+++ iterating_arrays.js
@@ -0,0 +1,22 @@
+const fruits =  ['apple', 'banana', 'cherry'];
+for(let i = 0; i <fruits.length; i++ ){
+    console.log(fruits[i])
+}
+
+const players = ['Alice', 'Bob', 'Charlie'];
+
+for(let i = 0; i <players.length; i++){
+    console.log(players[i]);
+};
+
+const flowers = [ 'rose', 'tulip', 'daisy'];
+
+flowers.forEach((flowers) => console.log(flowers));
+
+// using map in array
+
+const countries = ['USA', 'Canada', 'Mexico'];
+
+countries.map((country) => console.log(country));
+
+
