fffe78e035cacc5009a432c199dcd5274d5559a9
md abu hanif mia <mohammadbitullah@gmail.com>
2026-03-14 13:07:20 +0600

    learning array

---
+++ js_array.js
@@ -0,0 +1,9 @@
+const students = ["John", "Jane", "Doe"];
+
+// Accessing elements
+console.log(students[0]);
+console.log(students[1]);
+console.log(students[2]);
+// Adding elements
+students.push("Smith");
+console.log(students);
