1f1303bfebb41c86151a9e0956bcd811fd8e8bb5
md abu hanif mia <mohammadbitullah@gmail.com>
2026-03-15 01:49:52 +0600

    practice all loop method

--- loop.js
+++ loop.js
@@ -40,3 +40,42 @@ for(const key in person){
 
 };
 
+// for loop in array
+const numbers = [1, 2, 3, 4, 5];
+for(let i = 0; i <numbers.length; i++){
+    console.log(numbers[i]);
+
+};
+
+// for of loop in array
+
+const fruits = ['apple', 'banana', 'cherry'];
+for(const fruit of fruits){
+    console.log(fruit);
+};
+
+// forEach method in array
+const players = ['Alice', 'Bob', 'Charlie'];
+players.forEach((player) => {
+    console.log(player);
+});
+
+// break and continue in loop
+
+for(let i = 0; i <= 10; i++){
+    if(i === 5){
+        break;
+    }
+    console.log(i);
+};
+
+// continue statement
+
+for(let j = 0; j <= 10; j++){
+    if(j === 5){
+        continue;
+    };
+    console.log(j);
+};
+
+
