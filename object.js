95b9ef5b0010c12b6fe1034a5eebf96702568f23
md abu hanif mia <mohammadbitullah@gmail.com>
2026-03-14 22:30:51 +0600

    nested method in object

--- object.js
+++ object.js
@@ -28,3 +28,46 @@ const student = {
 };
 console.log(student);
 student.greet();
+
+// access, modify and delete object properties
+
+const book = {
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    page: 180,
+    publish: function(){
+        console.log(`${this.title} is written by ${this.author} and has ${this.page} pages.`)
+    }
+}
+// book.publish();
+// console.log(book.title);
+// book.title = 'The Great Gatsby - Revised Edition';
+// console.log(book.title);
+// delete book.page;
+// console.log(book);
+
+// update and modify object properties
+book.page = 200;
+console.log(book.page);
+
+book['title'] = 'The Great Gatsby - Revised Edition';
+console.log(book.title);
+
+// add new property to object
+book.genre = 'Novel';
+console.log(book.genre);
+
+// nested objects
+const person = {
+    name: 'Abu Hanif',
+    age: 27,
+    address: {
+        street: '123 Main St',
+        city: 'Dhaka',
+        country: 'Bangladesh'
+    }
+};
+console.log(person);
+console.log(person.name);
+console.log(person.age);
+console.log(person.address.city);
